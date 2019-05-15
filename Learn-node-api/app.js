40f5bf45fc0a92a85f60f55e92e3c88732159948
ashwani999 <ashwanisekhar999@gmail.com>
2019-05-15 11:31:56 +0530

    added mongoose connectivity

--- Learn-node-api/app.js
+++ Learn-node-api/app.js
@@ -2,9 +2,26 @@
 const express = require('express');
 const morgan =require('morgan');
 const postroutes=require('./routes/post')
+// import mongoose
+const mongoose = require('mongoose');
 var app = express();
 app.use(morgan("dev"));
 console.log("running");
 app.use("/",postroutes);
 
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+//localhost listen port
 app.listen(8000);
